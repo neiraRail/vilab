0897d4af8439ec6d589d786bd12fc604246399a5
Rail Neira <neira.rail@gmail.com>
2022-11-10 12:31:52 -0300

    Se agregan comentarios y parametros a integraciòn 1

--- Integration/1/1.ino
+++ Integration/1/1.ino
@@ -4,22 +4,26 @@
 #include <Adafruit_Sensor.h>
 #include<ESP8266_multipart.h>
 
-char* ssid     = "CEIS";
-char* password = "CEIS.2022";
+char* _ssid     = "CEIS";
+char* _password = "CEIS.2022";
+char* _server   = "192.168.1.107";
+int   _port     = 5000;
+
+int _windowTime = 2000;
 
 Adafruit_MPU6050 mpu;
-void setup() {
-  //Empezar y esperar el monitor serial
-  Serial.begin(115200);
-  while (!Serial)
-    delay(100);
-  delay(1000);
+
+void iniciarSPIFFS(){
+  Serial.println("Incializando SPIFFS...");
   if(!SPIFFS.begin(true)){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-  Serial.println(F("Inicializando sistema"));
-  //Comprobar chip MPU6050 acelerometro
+  Serial.println("SPIFFS montado correctamente");
+}
+
+void iniciarMPU(){
+  Serial.println("Buscando acelerómetro MPU6050...");
   if (!mpu.begin()) {
     Serial.println(F("Failed to find MPU6050 chip"));
     while (1) {
@@ -39,78 +43,100 @@ void setup() {
   mpu.setInterruptPinLatch(true);  // Keep it latched.  Will turn off when reinitialized.
   mpu.setInterruptPinPolarity(true);
   mpu.setMotionInterrupt(true);
+}
 
-  WiFi.begin(ssid, password);
+void iniciarWIFI(){
+  Serial.print("Intentando conectando a la siguiente red: ");
+  Serial.println(_ssid);
+  WiFi.begin(_ssid, _password);
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print(".");
   }
     Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("WiFi conectado");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  
 }
 
-int nro_files = 0;
+void setup() {
+  //Empezar y esperar el monitor serial
+  Serial.begin(115200);
+  while (!Serial)
+    delay(100);
+  delay(1000);
+  
+  iniciarSPIFFS();
+  iniciarMPU();
+  iniciarWIFI();  
+}
 
-File file;
+int nro_files = 0; //Contador de archivos enviados
+File file; //Archivo
 //Flag de evento
-boolean plottear = false;
-boolean hayFile = false;
+boolean evento = false;
+boolean hayFile = false; //Definirá si se debe enviar el archivo o capturar
 //contador de tiempo
 unsigned long start;
 void loop() {
-//  Serial.println("habrá file?: "+hayFile);
   if(!hayFile){
 //    Serial.println("NO");
     // Leer datos del acelerometro
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
   
-    //Acciones a realizar si está ocurriendo un evento
-    float acc;
-    if(plottear){
-      //calcular aceleración modulo de vector
-       acc = sqrt(a.acceleration.x*a.acceleration.x + a.acceleration.y*a.acceleration.y + a.acceleration.z*a.acceleration.z);
-       Serial.println(acc);
-       // Aqui debe ir la escritura de la lectura en un archivo.
-       file.println( "{ \"time\": \" " +(String)millis() + "\", \"acc\": \" "+acc+"\"}");
-    }
-  
-    
-    //Detectar evento y cambiar flag plottear
-    if(mpu.getMotionInterruptStatus() && !plottear){
+    // Detectar evento y cambiar flag evento
+    // Se crea un nuevo archivo
+    if(mpu.getMotionInterruptStatus() && !evento){
       start = millis();
-      plottear = true;
+      evento = true;
       //Abrir un archivo para escribir
       String filename = String("/evento")+nro_files+".txt";
       file = SPIFFS.open(filename, "w");
       if(!file){
         Serial.println("Fallo al abrir archivo para escribir");
-        plottear = false;
+        evento = false;
         return;
       }
       file.println("[");
+    } 
+    
+    
+    //Acciones a realizar si está ocurriendo un evento
+    if(evento){
+      // Puede ser una función que a partir de los sensor_event a, g y temp retorne el string que será escrito en el archivo.
+      //calcular aceleración modulo de vector
+      float acc = sqrt(a.acceleration.x*a.acceleration.x + a.acceleration.y*a.acceleration.y + a.acceleration.z*a.acceleration.z);
+      Serial.println(acc);
+      // Aqui debe ir la escritura de la lectura en un archivo.
+      file.println( "{ \"time\": \" " +(String)millis() + "\", \"acc\": \" "+acc+"\"}");
     }
-    //Finlizar evento despues de 3 segundos o dar 3 segundos más si en ese momento hay movimiento. Puede fallar por que aunque haya movimiento, podría calzar 
-    //justo un instante de no movimeinto
-    //Una alterantiva mejor puede ser llevar un contador y subirlo y bajarlo dependiendo de si hay movimiento o no, cambiar la flag cuando el contador llegue a cero
-    if(millis()-start > 2000 && plottear){
+    
+    //Finlizar evento despues de x segundos o dar x segundos más si en ese momento hay movimiento. 
+    //Puede fallar por que aunque haya movimiento, podría calzar justo un instante de no movimiento
+    //Una alterantiva mejor puede ser llevar un contador y subirlo y bajarlo dependiendo de si hay 
+    //movimiento o no, cambiar la flag cuando el contador llegue a cero.
+    //_windowTime es el tiempo mínimo de captura de evento.
+    if(millis()-start > _windowTime && evento){
+      // Finalizar evento
       if(!mpu.getMotionInterruptStatus()){
-        plottear = false;
+        evento = false;
         file.println("]");
         file.close();
         hayFile = true;
       }
+      //Si aún hay movimiento, se reinicia el contador para dar x segundos más.
       else
         start = millis();      
     }
-  
-    if(plottear){
+    
+    //Para escribir la coma, solo ni en este ciclo no se cerró el archivo.
+    if(evento){
       file.println(",\n");
     }
   }
+  // Esto se ejecuta cuando hay un archivo.
+  // La lectura se pausa hasta que el archivo se haya enviado.
   else{
     Serial.println("HAY FILE");
     String filename = String("/evento")+nro_files+".txt";
@@ -123,16 +149,19 @@ void loop() {
     }
     Serial.println("TODO beum abriendi eu arqivo");
     Serial.print("Mandando archivo......");
-    // put your setup code here, to run once:
-    ESP8266_multipart multipart("192.168.1.107"); // Sets the host to myexampleserver.com and the port to 80
-    multipart.setPort(5000); // Sets the port to 5000
+    ESP8266_multipart multipart(_server);
+    multipart.setPort(_port); // Sets the port to 5000
     int status = multipart.sendFile("/files", file);
     Serial.print(status);
     file.close();
 
-    hayFile = false;
-    nro_files++;
-    
-    delay(1000);
+    //Si el archivo fue enviado correctamente entonces eliminar archivo.
+    if(status!=0){
+      SPIFFS.remove(filename); //Retorna true si fue eliminado correctamente.
+      hayFile = false; //Volver a capturar datos
+      nro_files++; //Aumentar contador
+    }
+    else
+    ´Serial.println("No fue posible enviar el archivo, se intentarà nuevamente...");
   }
 }
